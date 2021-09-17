87323eaa16f4a388cab4758556002386b1f284d1
mjsML <mjs@aucegypt.edu>
2021-09-17 01:07:02 +0000

    more debug :(

--- examples/imagenet/train.py
+++ examples/imagenet/train.py
@@ -307,7 +307,7 @@ def train_and_evaluate(config: ml_collections.ConfigDict,
   else:
     steps_per_eval = config.steps_per_eval
 
-  steps_per_checkpoint = steps_per_epoch *10 
+  steps_per_checkpoint = steps_per_epoch #*10 
 
   base_learning_rate = config.learning_rate * config.batch_size / 256.
 
